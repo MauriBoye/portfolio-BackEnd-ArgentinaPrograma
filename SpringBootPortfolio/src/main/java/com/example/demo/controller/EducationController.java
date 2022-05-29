1ae2a165c74c3edb234e980739a6b68fcd336946
Mauri <mauriboye@gmail.com>
2022-05-28 21:27:13 -0300

    Full Education implementation

---
+++ SpringBootPortfolio/src/main/java/com/example/demo/controller/EducationController.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller;
+
+import com.example.demo.DTO.EducationDTO;
+import com.example.demo.services.IEducationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.rmi.ServerException;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("api/education")
+public class EducationController {
+
+    @Autowired
+    private IEducationService educationService;
+
+    @CrossOrigin
+    @GetMapping("/{id}")
+    public ResponseEntity<EducationDTO> findById(@PathVariable("id") Integer id){
+        EducationDTO educationDTO = educationService.findById(id);
+        return new ResponseEntity<>(educationDTO, HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @PostMapping("/create")
+    public ResponseEntity<EducationDTO> create(@RequestBody EducationDTO educationDTO){
+        EducationDTO newEducationDTO = educationService.create(educationDTO);
+        return new ResponseEntity<>(newEducationDTO, HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable("id") Integer id){
+        educationService.deleteById(id);
+        return new ResponseEntity<>("Education deleted", HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @PutMapping("/update")
+    public ResponseEntity<EducationDTO> update(@RequestBody EducationDTO educationDTO)throws ServerException{
+        if(educationService.findById(educationDTO.getId()) == null){
+            throw new ServerException("Education not found");
+        }else{
+            EducationDTO updateEducationDTO = educationService.update(educationDTO);
+            return new ResponseEntity<>(updateEducationDTO, HttpStatus.OK);
+        }
+    }
+
+    @CrossOrigin
+    @GetMapping("/list")
+    public Collection<EducationDTO> findAll() {
+        return educationService.findAll();
+    }
+}
